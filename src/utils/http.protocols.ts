e990497b765e0d242e85e48beb43421f3e3e8428
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-03-07 22:39:38 -0300

    feat: add http exceptions and http responses

---
+++ src/utils/http.protocols.ts
@@ -0,0 +1,25 @@
+import type { Response } from "express";
+
+class BaseResponse {
+  constructor(res: Response, statusCode: number, message?: unknown) {
+    res.status(statusCode).json(message);
+  }
+}
+
+export class StatusOK extends BaseResponse {
+  constructor(res: Response, message?: object | object[] | null) {
+    super(res, 200, message);
+  }
+}
+
+export class StatusOkNoContent extends BaseResponse {
+  constructor(res: Response) {
+    super(res, 204);
+  }
+}
+
+export class StatusCreated extends BaseResponse {
+  constructor(res: Response, message?: string | object) {
+    super(res, 201, message);
+  }
+}
