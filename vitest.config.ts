09bce712469767c1da894590063aeb344ecb0b51
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-03-12 20:35:33 -0300

    build: rename firstLayer to infrastructure

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     alias: {
       "@root": path.resolve(__dirname, "./"),
       "@src": path.resolve(__dirname, "./src/"),
-      "@inf": path.resolve(__dirname, "./src/firstLayer/"),
+      "@inf": path.resolve(__dirname, "./src/infrastructure/"),
       "@app": path.resolve(__dirname, "./src/application/"),
       "@dom": path.resolve(__dirname, "./src/domain/"),
       "@tes": path.resolve(__dirname, "./tests/"),
