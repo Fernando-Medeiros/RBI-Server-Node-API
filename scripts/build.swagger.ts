f5fe750e374251a8c50656d3887375cb400549dd
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-03-20 20:43:47 -0300

    docs: update swagger files

--- scripts/build.swagger.ts
+++ scripts/build.swagger.ts
@@ -10,14 +10,48 @@ import characterPaths from "docs/swagger/resources/character/paths.json";
 import characterReq from "docs/swagger/resources/character/requests.json";
 import characterRes from "docs/swagger/resources/character/responses.json";
 
+import statusPaths from "docs/swagger/resources/status/paths.json";
+import statusReq from "docs/swagger/resources/status/requests.json";
+import statusRes from "docs/swagger/resources/status/responses.json";
+
+import equipmentPaths from "docs/swagger/resources/equipment/paths.json";
+import equipmentReq from "docs/swagger/resources/equipment/requests.json";
+import equipmentRes from "docs/swagger/resources/equipment/responses.json";
+
+import inventoryPaths from "docs/swagger/resources/inventory/paths.json";
+import inventoryReq from "docs/swagger/resources/inventory/requests.json";
+import inventoryRes from "docs/swagger/resources/inventory/responses.json";
+
+import skillsPaths from "docs/swagger/resources/skills/paths.json";
+import skillsReq from "docs/swagger/resources/skills/requests.json";
+import skillsRes from "docs/swagger/resources/skills/responses.json";
+
 const build = new BuildSwagger();
 
 build.insertInfo([information]);
 
 build.insertSecurity([authentication]);
 
-build.insertComponents([exceptions, characterReq, characterRes]);
+build.insertComponents([
+  exceptions,
+  characterReq,
+  characterRes,
+  statusReq,
+  statusRes,
+  equipmentReq,
+  equipmentRes,
+  inventoryReq,
+  inventoryRes,
+  skillsReq,
+  skillsRes,
+]);
 
-build.insertPaths([characterPaths]);
+build.insertPaths([
+  characterPaths,
+  statusPaths,
+  equipmentPaths,
+  inventoryPaths,
+  skillsPaths,
+]);
 
 build.save();
