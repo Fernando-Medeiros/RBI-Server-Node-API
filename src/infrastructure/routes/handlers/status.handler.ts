8178a10ee485809e26bd6f459cfee3c04b42d982
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-17 10:49:24 -0300

    feat: StatusRepository -> private static readonly for all StatusHandler

--- src/infrastructure/routes/handlers/status.handler.ts
+++ src/infrastructure/routes/handlers/status.handler.ts
@@ -1,39 +1,39 @@
 import type { Request } from 'express';
 import { StatusRepository } from 'infra/repositories/status.repository.impl';
-import { StatusRequests } from 'infra/routes/requests/status.request';
+import { StatusRequests } from 'infra/routes/requests/status.request.impl';
 import { createCase } from 'app/use-cases/status-cases/create.case';
 import { deleteCase } from 'app/use-cases/status-cases/delete.case';
-import { getByIdCase } from 'app/use-cases/status-cases/get-by-id.case';
 import { updateCase } from 'app/use-cases/status-cases/update.case';
+import { getByIdCase } from 'app/use-cases/status-cases/get-by-id.case';
 
 export class StatusHandler {
-    private readonly Repository = new StatusRepository();
+    private static readonly Repository = new StatusRepository();
 
     async getStatusById(req: Request) {
         return await getByIdCase(
             new StatusRequests({ ...req.params, ...req.body }),
-            this.Repository,
+            StatusHandler.Repository,
         );
     }
 
     async createStatus(req: Request) {
         return await createCase(
             new StatusRequests({ ...req.headers, ...req.body }),
-            this.Repository,
+            StatusHandler.Repository,
         );
     }
 
     async deleteStatus(req: Request) {
         return await deleteCase(
             new StatusRequests({ ...req.headers, ...req.body }),
-            this.Repository,
+            StatusHandler.Repository,
         );
     }
 
     async updateStatus(req: Request) {
         return await updateCase(
             new StatusRequests({ ...req.headers, ...req.body }),
-            this.Repository,
+            StatusHandler.Repository,
         );
     }
 }
