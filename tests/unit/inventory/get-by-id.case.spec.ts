1ef37c506f16c7a52fa9bc44b477581b3b27e21d
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-07-22 21:30:29 -0300

    tests: refactor -> Unit -> Inventory

--- tests/unit/inventory/get-by-id.case.spec.ts
+++ tests/unit/inventory/get-by-id.case.spec.ts
@@ -1,14 +1,17 @@
 import { describe, it, expect } from 'vitest';
-import { InMemoryInventoryRepository } from './mock/inMemoryInventoryRepository';
+import { InMemoryInventoryRepository } from './mock/in-memory.inventory.repository';
 import { InventoryRequests } from 'infra/routes/requests/inventory.request.impl';
 import { getByIdCase } from 'app/use-cases/inventory-cases/get-by-id.case';
+import { BadRequest, NotFound } from 'utils/http.exceptions';
+import * as uuid from 'uuid';
 
 const repository = new InMemoryInventoryRepository();
-const { id } = repository.helpers.pubId();
+
+const { pubId: id } = repository.getDataMock();
+
+repository.save(repository.getDataMock());
 
 describe('Get-By-Id-> Inventory-OK', () => {
-    repository.helpers.insertOneToDatabase();
-
     it('Should get a Inventory by id', async () => {
         const res = await getByIdCase(
             new InventoryRequests({ id }),
@@ -20,26 +23,19 @@ describe('Get-By-Id-> Inventory-OK', () => {
 });
 
 describe('Get-By-Id-> Inventory-Exceptions', () => {
-    it('Should return error when passing an invalid id', async () => {
-        await expect(() =>
-            getByIdCase(new InventoryRequests({ id: '000-000' }), repository),
-        ).rejects.toThrowError('Could not verify credentials');
+    it('Should return [BadRequest] when passing an invalid id', () => {
+        const usingArrayIds = [{ id: '' }, { id: '000-000' }];
+
+        usingArrayIds.forEach(async testId => {
+            await expect(() =>
+                getByIdCase(new InventoryRequests({ ...testId }), repository),
+            ).rejects.toThrowError(BadRequest);
+        });
     });
 
-    it('Should return error when passing an null id', async () => {
+    it('Should return [NotFound] when entering a valid but non-existent id', async () => {
         await expect(() =>
-            getByIdCase(new InventoryRequests({ id: '' }), repository),
-        ).rejects.toThrowError('Could not verify credentials');
-    });
-
-    it('Should return [not found] when entering a valid but non-existent id', async () => {
-        await expect(() =>
-            getByIdCase(
-                new InventoryRequests({
-                    id: 'b2cd80d6-825d-4b67-a7a5-3cface4f19b9',
-                }),
-                repository,
-            ),
-        ).rejects.toThrowError('not found');
+            getByIdCase(new InventoryRequests({ id: uuid.v4() }), repository),
+        ).rejects.toThrowError(NotFound);
     });
 });
