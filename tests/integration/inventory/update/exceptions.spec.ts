e47914f33f712e1d4079d90499855a58251508bc
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-07-22 23:47:48 -0300

    Tests inventory (#9)
    * tests: refactor -> Unit -> Inventory
    
    * tests: refactor -> Integration -> Inventory

--- tests/integration/inventory/update/exceptions.spec.ts
+++ tests/integration/inventory/update/exceptions.spec.ts
@@ -7,6 +7,16 @@ import { Helpers } from 'tests/config/helpers/insert-remove';
 const mock = new InventoryMock();
 const headers = { ...secretHeader, Authorization: '' };
 
+function usingArrayItems<T>(value: T[]) {
+    return [
+        { armors: value },
+        { accessories: value },
+        { consumables: value },
+        { materials: value },
+        { weapons: value },
+    ];
+}
+
 describe('Inventory - Update - Exceptions', async () => {
     beforeAll(async () => {
         Object.assign(
@@ -23,48 +33,33 @@ describe('Inventory - Update - Exceptions', async () => {
         await Helpers.removeAfterAll('/inventories', headers);
     });
 
-    it('Should return 400 when trying to update invalid armors', async () => {
-        const toUpdate = { armors: [{ name: '', type: '' }] };
-
-        const res = await app.patch('/inventories').send(toUpdate).set(headers);
+    it('Should return 400 when trying to update NULL', async () => {
+        const res = await app.patch('/inventories').send().set(headers);
 
         expect(res.statusCode).toEqual(400);
         expect(res.body).toHaveProperty('message');
     });
 
-    it('Should return 400 when trying to update invalid accessories', async () => {
-        const toUpdate = { accessories: [{ name: '', type: '' }] };
+    it('Should return 400 when trying to update INVALID ITEM', () => {
+        usingArrayItems<object>([{ invalidField: '', value: -11 }]).forEach(
+            async element => {
+                const res = await app
+                    .patch('/inventories')
+                    .send(element)
+                    .set(headers);
 
-        const res = await app.patch('/inventories').send(toUpdate).set(headers);
-
-        expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty('message');
+                expect(res.statusCode).toEqual(400);
+                expect(res.body).toHaveProperty('message');
+            },
+        );
     });
 
-    it('Should return 400 when trying to update invalid consumables', async () => {
-        const toUpdate = { consumables: [{ name: '', type: '' }] };
+    it('Should return 401 when sending invalid or null token', async () => {
+        const res = await app
+            .patch('/inventories')
+            .set({ ...secretHeader, Authorization: 'Bearer 000000' });
 
-        const res = await app.patch('/inventories').send(toUpdate).set(headers);
-
-        expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty('message');
-    });
-
-    it('Should return 400 when trying to update invalid materials', async () => {
-        const toUpdate = { materials: [{ name: '', type: '' }] };
-
-        const res = await app.patch('/inventories').send(toUpdate).set(headers);
-
-        expect(res.statusCode).toEqual(400);
-        expect(res.body).toHaveProperty('message');
-    });
-
-    it('Should return 400 when trying to update invalid weapons', async () => {
-        const toUpdate = { weapons: [{ name: '', type: '' }] };
-
-        const res = await app.patch('/inventories').send(toUpdate).set(headers);
-
-        expect(res.statusCode).toEqual(400);
+        expect(res.statusCode).toEqual(401);
         expect(res.body).toHaveProperty('message');
     });
 });
